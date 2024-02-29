68c2b16e344f75ce75eebf665b94304e2011fb41
Pillow <fagodfagod@gmail.com>
2024-02-29 22:37:20 +0200

    build(ui): init spartan-ng/ui

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,10 +1,10 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
+  presets: [require('@spartan-ng/ui-core/hlm-tailwind-preset')],
   content: [
-    "./src/**/*.{html,ts}",
+    './src/**/*.{html,ts}',
+    './src/components/**/*.{html,ts}',
   ],
-  theme: {
-    extend: {},
-  },
   plugins: [],
-}
+};
