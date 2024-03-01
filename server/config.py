319d1ffd5e3fccd9db52a5add51f31b81d027e31
Pillow <fagodfagod@gmail.com>
2024-03-01 21:45:28 +0200

    feat: cors policy

--- server/config.py
+++ server/config.py
@@ -4,3 +4,4 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+CLIENT_URL = os.getenv("CLIENT_URL")
--- server/main.py
+++ server/main.py
@@ -1,11 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from tortoise.contrib.fastapi import register_tortoise
 
-from server.config import DATABASE_URL
-
+from server.config import DATABASE_URL, CLIENT_URL
 from server.routers import person_router, crime_router
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[CLIENT_URL],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(person_router)
 app.include_router(crime_router)
 
@@ -15,5 +23,3 @@ register_tortoise(
     modules={"models": ["server.models"]},
     generate_schemas=True,
 )
-
-
